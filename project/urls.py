7ff19b8d2e2f715ac00a38d21a5da81a1bfb81d4
Kiana8351 <kfaramehr@gmail.com>
2024-08-21 23:45:24 +0330

    add test

--- project/urls.py
+++ project/urls.py
@@ -5,5 +5,6 @@ app_name = 'project'
 urlpatterns = [
     path('',home_page,name='index'),
     path('about',about_page,name='about'),
-    path('contact',contact_page,name='contact')
+    path('contact',contact_page,name='contact'),
+    path('test',test_view,name='test')
 ]
--- project/views.py
+++ project/views.py
@@ -7,3 +7,6 @@ def about_page(request):
     return render(request,'about.html')
 def contact_page(request):
     return render(request,'contact.html')
+
+def test_view(request):
+    return render(request,'test.html')
